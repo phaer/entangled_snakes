66bef9aad901d9ed7382b07ef88b356983dd6e3d
phaer <hello@phaer.org>
2024-03-27 12:36:35 +0100

    editable: add python  prefix, improve shellHook

--- lib/editable.nix
+++ lib/editable.nix
@@ -52,20 +52,29 @@ let
 
           out = Path(os.getenv("out"))
           out.mkdir()
-
-          editable_source = Path("${projectRoot}")
+          site_packages = out / "${python.sitePackages}"
+          site_packages.mkdir(parents=True)
 
           src = Path(os.getenv("src"))
+
+          editable_source = Path("${projectRoot}")
+          # TODO Replace this by a better heuristic to find out whether the
+          # current project uses src layout or not.
+          # We check "src" (in the nix store) because editable_source isn't accessible
+          # in the sandbox
+          if (src / "src").exists():
+            editable_source = editable_source / "src"
+
           os.chdir(src)
           # Call the build-backend as per PEP660
-          dist_info = ${backend}.prepare_metadata_for_build_editable(out)
+          dist_info = ${backend}.prepare_metadata_for_build_editable(site_packages)
 
           # remove .egg_info if it exists, as it's unecessary and might confuse other tools.
-          for egg_info in out.glob("*.egg-info"):
+          for egg_info in site_packages.glob("*.egg-info"):
             shutil.rmtree(egg_info)
 
           # write direct_url.json as per PEP 660
-          direct_json_path = out / dist_info / "direct_url.json"
+          direct_json_path = site_packages / dist_info / "direct_url.json"
           with open(direct_json_path, 'w') as f:
             data = {
               "url": f"file://{editable_source}",
@@ -75,31 +84,24 @@ let
             }
             json.dump(data, f, indent=2)
 
-          # TODO Replace this by a better heuristic to find out whether the
-          # current project uses src layout or not.
-          if (src / "src").exists():
-            editable_source = editable_source / "src"
-
           # write .pth file, commented out because we don't
           # use it atm.
-          #pth_path = Path(out / dist_info).with_suffix('.pth')
-          #with open(pth_path, 'w') as f:
-          #  f.write(f"{editable_source}\n")
+          pth_path = (site_packages / dist_info).with_suffix('.pth')
+          with open(pth_path, 'w') as f:
+            f.write(f"{editable_source}\n")
 
           # get console_scripts from entrypoints
-          distribution = importlib.metadata.Distribution.at(out / dist_info)
+          distribution = importlib.metadata.Distribution.at(site_packages / dist_info)
           console_scripts = distribution.entry_points.select(group='console_scripts')
-
           # TODO support other entrypoints
 
-          # write editable.sh
           # TODO shellhook
-          with open(out / "editable.sh", "w") as f:
+          with open(out / "shellHook.sh", "w") as f:
             aliases = "\n".join([
               f"alias {script.name}='${python}/bin/python -m {script.value}'"
               for script in console_scripts])
             f.write(f"""
-              export PYTHONPATH={editable_source}:{out}:$PYTHONPATH
+              export PYTHONPATH={site_packages}:$PYTHONPATH
               {aliases}
             """)
         ''
